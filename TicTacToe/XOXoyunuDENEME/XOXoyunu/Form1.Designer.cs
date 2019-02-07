0599fc094cfe630afe4f1bb929dd2f1fba13fd57
MineYaman <47187448+MineYaman@users.noreply.github.com>
2019-02-07 23:13:15 +0300

    XOXoyunu

---
+++ TicTacToe/XOXoyunuDENEME/XOXoyunu/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace XOXoyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("AR DARLING", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(167, 302);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(299, 98);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Font = new System.Drawing.Font("AR DARLING", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHelp.Location = new System.Drawing.Point(167, 198);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(299, 98);
+            this.btnHelp.TabIndex = 1;
+            this.btnHelp.Text = "HELP";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Font = new System.Drawing.Font("AR DARLING", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewGame.Location = new System.Drawing.Point(167, 94);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(299, 98);
+            this.btnNewGame.TabIndex = 0;
+            this.btnNewGame.Text = "NEW GAME";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 459);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.btnNewGame);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ANA MENÜ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Button btnNewGame;
+    }
+}
+
---
+++ TicTacToe/XOXoyunuDENEME7/XOXoyunu/OyunSecenekleri.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+//using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+namespace XOXoyunu
+{
+    public partial class OyunSecenekleri : Form
+    {
+        public static void Main(string[] args)
+        {
+            IPAddress ip = Dns.GetHostEntry("LOCALHOST").AddressList[0];
+            TcpListener server = new TcpListener(ip, 8080);
+            TcpClient client = default(TcpClient);
+            try
+            {
+                server.Start();
+                Console.WriteLine("server started ....");
+                Console.Read();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.Read();
+                //throw;
+            }
+
+            while (true)
+            {
+                client = server.AcceptTcpClient();
+
+                NetworkStream stream = client.GetStream();
+            }
+        }
+
+        public OyunSecenekleri()
+        {
+            InitializeComponent();
+        }
+
+        private void rdbtnOnePlayer_CheckedChanged(object sender, EventArgs e)
+        {
+            lblPlayerOne.Visible = true;
+            txtPlayerOne.Visible = true;
+            lblPlayerTwo.Visible = false;
+            txtPlayerTwo.Visible = false;
+            txtPlayerTwo.Text = "";
+            btnPlay.Visible = true;
+        }
+
+        private void rdbtnTwoPlayer_CheckedChanged(object sender, EventArgs e)
+        {
+            lblPlayerOne.Visible = true;
+            txtPlayerOne.Visible = true;
+            lblPlayerTwo.Visible = true;
+            txtPlayerTwo.Visible = true;
+            btnPlay.Visible = true;
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            frmXOX xox1 = new frmXOX();
+            frmXOX.PlayerNames(txtPlayerOne.Text, txtPlayerTwo.Text);
+            xox1.ShowDialog();
+            this.Close();
+        }
+
+        private void txtPlayerTwo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar.ToString() == "\r")
+                btnPlay.PerformClick();
+        }
+    }
+}
